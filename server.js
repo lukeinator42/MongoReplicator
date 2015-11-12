2bda071909b82995118e47aed59d8cc6a0d480ac
Lukas Grasse <lukas@MacBook-Air.local>
2015-11-11 20:03:09 -0700

    all routes except update and sync

--- server.js
+++ server.js
@@ -36,12 +36,27 @@ app.get('/', function (req, res) {
 	});
 });
 
+//get logs that are more recent than parameter id
+app.get('/log/:id', function (req, res) {
+Log.find({logId: { $gt: req.params.id } }).exec(function (err, log) {
+		if (err) return handleError(err);
+		  res.send(log);
+	});
+});
+
 //display log
 app.get('/log', function (req, res) {
 
 	Log.find().exec(function (err, log) {
 		if (err) return handleError(err);
 		  res.send(log);
+	});
+});
+
+app.get('/doc/:id', function (req, res) {
+Note.find({_id: req.params.id }).exec(function (err, note) {
+		if (err) return handleError(err);
+		  res.send(note);
 	});
 });
 
@@ -87,9 +102,49 @@ app.post('/doc', function(req, res) {
 			});
 	    }
 	);
+});
 
-	
+//delete a doc from the server
+app.delete('/doc/:id', function(req, res) { 
+	console.log(req.body);
 
+	//delete doc
+	Note.remove({_id: req.params.id }, function(err) {
+	if (err) // ...
+	  	res.send('error:' + err);
+	else
+		res.send("success!");
+	});
+
+	Log.findOne()
+	    .sort({logId: -1})
+	    .exec(function(err, doc)
+	    {
+
+	    	var newLogId;
+	    	if(doc!=null)
+	    	 	newLogId = doc.logId+1;
+	    	 else
+	    	 	newLogId = 0;
+	        // ...
+	        console.log(newLogId);
+
+			//log posted doc
+			var log = new Log({logId: newLogId, 
+							   objectId: req.params.id, 
+							   eventType: "Delete", 
+							   timestamp: new Date, 
+							   version: 1, 
+							   random: Math.random()});
+
+			log.save(function (err) {
+			  if (err) // ...
+			  	console.log('error:' + err);
+			  else
+				console.log("Delete logged" + log);
+			});
+	    }
+	);
 });
 
 app.get('/deleteAllLogs', function (req, res) {
@@ -97,7 +152,7 @@ app.get('/deleteAllLogs', function (req, res) {
 	if (err) // ...
 	  	res.send('error:' + err);
 	else
-		res.send("success!" + note);
+		res.send("success!");
 	});
 });
 
@@ -106,7 +161,7 @@ app.get('/deleteAllNotes', function (req, res) {
 	if (err) // ...
 	  	res.send('error:' + err);
 	else
-		res.send("success!" + note);
+		res.send("success!");
 	});
 });
 
@@ -115,5 +170,5 @@ var server = app.listen(process.argv[3], function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at %s', port);
+  console.log('ReplicatorServer lisening at %s', port);
 });
